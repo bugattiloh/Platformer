14904bbab751fe1d31955b2e7d6cb0d4607ae66f
bugattiloh <bugattiloh2000@gmail.com>
2022-03-21 00:05:47 +0300

    update Mace

---
+++ Assets/Scripts/MaceScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MaceScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    private bool _isUpMoving;
+   
+
+
+    private void Move()
+    {
+        Vector3 direction;
+        if (_isUpMoving)
+        {
+            direction = transform.up * speed;
+        }
+        else
+        {
+            direction = -transform.up * speed;
+        }
+
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = currentPos + direction;
+
+        transform.position = Vector3.MoveTowards(currentPos, targetPos, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (collision.CompareTag("Ground"))
+        {
+            _isUpMoving = true;
+        }
+        if (collision.CompareTag("Point"))
+        {
+            _isUpMoving = false;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+    }
+}
