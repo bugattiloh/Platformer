4bedab8dc9f2c62c921b669b6d202b3fe94ed3a9
bugattiloh <bugattiloh2000@gmail.com>
2022-03-22 02:18:09 +0300

    added Audio Options

---
+++ Assets/Scripts/OptionMenuScript.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionMenuScript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void Sound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+}
